fbd0a2f7db0199fb9adc5de0f400e8d5afd898a6
W|H Designs <will@rhinorea.com>
2019-04-30 20:51:30 -0400

    Adding changes to leaderboard storyboard

--- Game459/Controllers/LeaderboardControllerTableViewController.swift
+++ Game459/Controllers/LeaderboardControllerTableViewController.swift
@@ -14,7 +14,7 @@ class LeaderboardControllerTableViewController: UITableViewController {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
@@ -26,6 +26,8 @@ class LeaderboardControllerTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
+    
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
